9d05deaa26b9c1aac74b25c7f93ac3c4919a3167
Thanatat Wongabut <thanatat.wong@mail.kmutt.ac.th>
2021-06-12 00:27:42 +0700

    [ADD] Firebase for Android

---
+++ lib/Pages/SettingPage/Setting.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+ Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0XFF3EAF51),
+        
+    );
+  }
+}
