ea3a64d8a8dd4b8d231fd43ce0984655c73e9bd3
Sakkarin2000 <sakkarin.kmutt@mail.kmutt.ac.th>
2021-06-16 02:36:25 +0700

    [Update] Dialog Statemanagement

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -29,6 +29,7 @@ class _HomeState extends State<Home> {
       topRight: Radius.circular(25.0),
     );
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         extendBodyBehindAppBar: true,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
