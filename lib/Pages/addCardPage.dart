3b8f70e1fe34f65ae665be42404b66d44d835b8c
Parinthon Puksuriwong <nungwin@Macintosh.local>
2021-06-21 02:07:51 +0700

    [ADD]: add card page

--- lib/Pages/addCardPage.dart
+++ lib/Pages/addCardPage.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:credit_card_validate/credit_card_validate.dart';
 
-class AddCardPage extends StatelessWidget {
+class AddCardPage extends StatefulWidget {
+  @override
+  State<AddCardPage> createState() => _AddCardPageState();
+}
+
+class _AddCardPageState extends State<AddCardPage> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  String cardType = '';
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +31,124 @@ class AddCardPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       backgroundColor: Color(0xff3EAF51),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                "กรอกข้อมูลบัตร",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              height: 650,
+              child: Card(
+                child: Container(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: <Widget>[
+                      CreditCardWidget(
+                        cardNumber: cardNumber,
+                        expiryDate: expiryDate,
+                        cardHolderName: cardHolderName,
+                        cvvCode: cvvCode,
+                        showBackView: isCvvFocused,
+                        obscureCardNumber: true,
+                        obscureCardCvv: true,
+                        cardBgColor: Colors.black,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            CreditCardForm(
+                              formKey: formKey,
+                              obscureCvv: true,
+                              obscureNumber: true,
+                              cardNumber: cardNumber,
+                              cvvCode: cvvCode,
+                              cardHolderName: cardHolderName,
+                              expiryDate: expiryDate,
+                              themeColor: Colors.blue,
+                              cardNumberDecoration: const InputDecoration(
+                                fillColor: Colors.white,
+                                filled: true,
+                                border: OutlineInputBorder(),
+                                labelText: 'Number',
+                                hintText: 'XXXX XXXX XXXX XXXX',
+                              ),
+                              expiryDateDecoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Expired Date',
+                                hintText: 'XX/XX',
+                              ),
+                              cvvCodeDecoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'CVV',
+                                hintText: 'XXX',
+                              ),
+                              cardHolderDecoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Card Holder',
+                              ),
+                              onCreditCardModelChange: onCreditCardModelChange,
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(top: 20),
+                              child: TextButton(
+                                  onPressed: () {
+                                    if (formKey.currentState.validate()) {
+                                      print('valid!');
+                                      print(
+                                          CreditCardValidator.identifyCardBrand(
+                                              cardNumber));
+                                    } else {
+                                      print('invalid!');
+                                      print(
+                                          CreditCardValidator.identifyCardBrand(
+                                              cardNumber));
+                                    }
+                                  },
+                                  style: TextButton.styleFrom(
+                                      minimumSize: Size(
+                                          MediaQuery.of(context).size.width *
+                                              0.9,
+                                          MediaQuery.of(context).size.height *
+                                              0.055),
+                                      primary: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20.0)),
+                                      backgroundColor: Color(0xffDD582D)),
+                                  child: Text(
+                                    'Submit',
+                                    style: TextStyle(fontSize: 15),
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void onCreditCardModelChange(CreditCardModel creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
   }
 }
